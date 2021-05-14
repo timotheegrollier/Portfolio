7e9b0efe0f4309e1b7ee3e1de40146d5ff07fc36
Timothee GROLLIER <timotheegrollier@gmail.com>
2021-05-14 15:18:28 +0200

    Ajout de la page compétences

--- src/components/knowledges/Hobbies.js
+++ src/components/knowledges/Hobbies.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Hobbies = () => {
+  let theme = localStorage.getItem("theme");
+  useEffect(() => {
+    let hobbies = document.getElementById("hob");
+    if (theme === "settingsBlock dark-theme") {
+      hobbies.classList.add("dark-theme");
+    }
+  });
+
   return (
-    <div>
-      <p>Hobbies</p>
+    <div className="hobbies" id="hob">
+      <h3>Intérêts</h3>
+      <ul>
+        <li className="hobby">
+          <i className="fas fa-music">
+            <span>Guitare/M.A.O</span>
+          </i>
+        </li>
+        <li className="hobby">
+          <i className="fas fa-water">
+            <span>Bodyboard</span>
+          </i>
+        </li>
+        <li className="hobby">
+          <i className="fas fa-hat-wizard">
+            <span>Jeux-vidéo</span>
+          </i>
+        </li>
+      </ul>
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,35 +3,33 @@ import Navigation from "../components/Navigation";
 
 const Home = () => {
   // FUNCTIONS
-  
-  let theme = localStorage.getItem("theme")
 
+  let theme = localStorage.getItem("theme");
 
-
-useEffect(() => {  
-  let home = document.getElementById("home")
-  if(theme === "settingsBlock dark-theme"){
-    home.classList.add("dark-theme")
-  }
+  useEffect(() => {
+    let home = document.getElementById("home");
+    if (theme === "settingsBlock dark-theme") {
+      home.classList.add("dark-theme");
+    }
   });
 
-  // 
+  //
   return (
     <div className="home">
       <Navigation />
-      <div className="homeContent" id="home" >
-        <div className="content" >
-          <h1 >Timothée Grollier</h1>
+      <div className="homeContent" id="home">
+        <div className="content">
+          <h1>Timothée Grollier</h1>
           <h2>Développeur Full-Stack</h2>
           <div className="pdf">
-          <a href="./media/TimotheeGROLLIER(4).pdf" target="_blank">Télécharger CV</a>
+            <a href="./media/TimotheeGROLLIER(4).pdf" target="_blank">
+              Télécharger CV
+            </a>
           </div>
         </div>
       </div>
     </div>
   );
-  
 };
-
 
 export default Home;
