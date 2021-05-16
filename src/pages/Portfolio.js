4fb11ae1fb62685cdc38c3e77b303a7201cbb45b
Timothee GROLLIER <timotheegrollier@gmail.com>
2021-05-16 11:02:16 +0200

    Ajout des radios + theme

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,21 +1,22 @@
-import React , { useEffect } from "react";
+import React, { useEffect } from "react";
 import Navigation from "../components/Navigation";
 import ProjectList from "../components/portfolio/ProjectList";
 
 const Portfolio = () => {
-let theme = localStorage.getItem("theme")
-useEffect(()=>{
-  let works = document.getElementsByClassName("project")
-  if (theme === "settingsBlock dark-theme") {
-    works[0].classList.add("dark-theme")
-    works[1].classList.add("dark-theme")
-    works[2].classList.add("dark-theme")
-    works[3].classList.add("dark-theme")
-    works[4].classList.add("dark-theme")
-    works[5].classList.add("dark-theme")
-  }
-})
-
+  let theme = localStorage.getItem("theme");
+  useEffect(() => {
+    let works = document.getElementsByClassName("project");
+    let radioDisplay = document.querySelector(".radioDisplay");
+    if (theme === "settingsBlock dark-theme") {
+      works[0].classList.add("dark-theme");
+      works[1].classList.add("dark-theme");
+      works[2].classList.add("dark-theme");
+      works[3].classList.add("dark-theme");
+      works[4].classList.add("dark-theme");
+      works[5].classList.add("dark-theme");
+      radioDisplay.classList.add("modal");
+    }
+  });
 
   return (
     <div className="portfolio">
