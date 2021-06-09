7c363be079473846984ff982b246dbf2d4628b4b
Timothee GROLLIER <timotheegrollier@gmail.com>
2021-06-10 01:45:31 +0200

    add Nous & modify projects

--- src/components/knowledges/Experience.js
+++ src/components/knowledges/Experience.js
@@ -5,6 +5,14 @@ const Experience = () => {
     <div className="experience" id="xp">
       <h3>Expérience</h3>
       <div className="exp1">
+        <h4>Stage Developpeur Web agence: "NOUS:Ouvert,Utile et Simple" </h4>
+        <h5>Juin/Septembre 2021</h5>
+        <ul>
+          <li>Création de bloc WordPress JSX</li>
+          <li>Wordpress</li>
+        </ul>
+      </div>
+      <div className="exp2">
         <h4>Caissier - Super U Lacanau</h4>
         <h5>2017-2019</h5>
         <ul>
